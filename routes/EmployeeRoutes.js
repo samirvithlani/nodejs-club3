7e3fa3c1240cbfbdfc10fa6b49d958a32ada5d06
samirvithlani <samir.vithlani83955@gmail.com>
2023-01-31 19:37:06 +0530

    add

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controller/EmployeeController");
-const Authmiddleware = require("../middleware/AuthMiddleware");
-const zodMiddleware = require("../middleware/ZodMiddleware");
+// const Authmiddleware = require("../middleware/AuthMiddleware");
+// const zodMiddleware = require("../middleware/ZodMiddleware");
 //const employeeValidationSchema = require("../util/EmployeeSchemaValidation");
 //const auth = require("../middleware/Auth1Middleware");
 
