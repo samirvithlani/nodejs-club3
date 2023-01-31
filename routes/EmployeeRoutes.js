69ecfba3cd0fc57c6010850baacb8dd3998cb706
samirvithlani <samir.vithlani83955@gmail.com>
2023-01-31 19:33:36 +0530

    add

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const employeeController = require("../controller/EmployeeController");
 const Authmiddleware = require("../middleware/AuthMiddleware");
 const zodMiddleware = require("../middleware/ZodMiddleware");
-const employeeValidationSchema = require("../util/EmployeeSchemaValidation");
-const auth = require("../middleware/Auth1Middleware");
+//const employeeValidationSchema = require("../util/EmployeeSchemaValidation");
+//const auth = require("../middleware/Auth1Middleware");
 
-router.post('/employee',zodMiddleware.validate(employeeValidationSchema),employeeController.addEmployee)
-router.get('/employee',auth.validate,employeeController.getAllEmployee)
+//router.post('/employee',zodMiddleware.validate(employeeValidationSchema),employeeController.addEmployee)
+//router.get('/employee',auth.validate,employeeController.getAllEmployee)
 
 module.exports = router;
